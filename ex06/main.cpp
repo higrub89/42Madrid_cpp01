7563eca80becbde1b9e2c6c1ebc7370bc19a7216
Ruben Dario Higuita Rubio <higuitaruben@hotmail.com>
2026-01-18 18:31:48 +0100

    feat: complete ex06 Harl Filter with switch logic

---
+++ ex06/main.cpp
@@ -0,0 +1,43 @@
+#include "Harl.hpp"
+
+int getLevelIndex(std::string level)
+{
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (levels[i] == level)
+            return i;
+    }
+    return (-1);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+        return (1);
+    }
+
+    Harl harl;
+    int index = getLevelIndex(argv[1]);
+
+    switch (index)
+    {
+        case 0:
+            harl.complain("DEBUG");
+            // fall through
+        case 1:
+            harl.complain("INFO");
+            // fall through
+        case 2:
+            harl.complain("WARNING");
+            // fall through
+        case 3:
+            harl.complain("ERROR");
+            break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    }
+}
